67cb7d75889e25149e5be689bbf8f920f0eb7af5
Ilze Upmane <ilze.upmane@inbox.lv>
2022-09-18 00:24:46 +0300

    dd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,18 @@ def home():
 @app.route('/iepriekseja')
 def iepriekseja():
    return render_template('iepriekseja.html')
+
+@app.route('/nakama')
+def nakama():
+   return render_template('nakama.html')
+
+@app.route('/parametri')
+def parametri():
+    id = request.args.get('id')
+    title = request.args.get('title')
+    print("ID:", id)
+    print("Title:", title)
+    return "Param"
 
 if __name__ == "__main__":
    app.run(debug = True)
